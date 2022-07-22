155c4fe44886352711e50c13142d200688a8eedc
awitas <awitas@vindicotech.com>
2022-07-22 12:27:19 -0700

    bump version

--- gateway/runtime/apigw/doc.go
+++ gateway/runtime/apigw/doc.go
@@ -1,2 +1,18 @@
 // Package apigw defines apigw lambda datly runtime
 package apigw
+
+import (
+	"fmt"
+)
+
+func f() error {
+
+	var in interface{} = 123
+	fromJSONVal, ok := in.(float64)
+	if !ok {
+		return fmt.Errorf("expected %T , but hS: %T", fromJSONVal, in)
+	}
+	i := int(fromJSONVal)
+	fmt.Printf("%v\n", i)
+	return nil
+}
