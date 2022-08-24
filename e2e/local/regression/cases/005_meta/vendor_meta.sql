cba098a54070f2852135bbbbe6ca88ceb60f60fd
awitas <awitas@vindicotech.com>
2022-08-24 15:18:55 -0700

    updated template meta case

--- e2e/local/regression/cases/005_meta/vendor_meta.sql
+++ e2e/local/regression/cases/005_meta/vendor_meta.sql
@@ -1,7 +1,7 @@
 /* {"URI":"meta/vendors/"} */
 SELECT vendor.*,
        products.* EXCEPT VENDOR_ID,
-       Meta.*   /* {"Output":{"In":"Header", "Name":"Vendor-Meta", "Case":"lc"}} */
+       Meta.*   
 FROM (SELECT t.* FROM VENDOR t WHERE 1=1  ) vendor
     JOIN (SELECT * FROM PRODUCT t) products ON products.VENDOR_ID = vendor.ID
     JOIN (SELECT 1 + (COUNT(1) / $View.Limit) AS PAGE_CNT, COUNT(1) AS TOTAL_COUNT
