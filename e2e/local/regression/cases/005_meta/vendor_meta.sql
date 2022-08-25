686df65ebd229207a3f9b5e3fa377fee3cbd5efe
awitas <awitas@vindicotech.com>
2022-08-25 15:03:37 -0700

    updated test case

--- e2e/local/regression/cases/005_meta/vendor_meta.sql
+++ e2e/local/regression/cases/005_meta/vendor_meta.sql
@@ -1,8 +1,8 @@
 /* {"URI":"meta/vendors/"} */
-SELECT vendor.* /* {"Style":"Comprehensive"}  */,
+SELECT vendor.* /* {"Style":"Comprehensive", "ResponseField":"Data"}  */,
        products.* EXCEPT VENDOR_ID,
-       Meta.*
+       Meta.* /* {"Kind": "record"} */
 FROM (SELECT t.* FROM VENDOR t WHERE 1=1  ) vendor
-    JOIN (SELECT * FROM PRODUCT t) products ON products.VENDOR_ID = vendor.ID
+    JOIN (SELECT * FROM PRODUCT t) products  ON products.VENDOR_ID = vendor.ID
     JOIN (SELECT   CAST(1 + (COUNT(1) / $View.Limit) AS SIGNED)  AS PAGE_CNT, COUNT(1) AS CNT
     FROM ($View.vendor.SQL)  t ) AS Meta ON 1=1
