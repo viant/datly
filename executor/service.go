bf34caad33159db184c9f95475210a48f05a1e8c
klarysz <klarysz@viantinc.com>
2022-09-06 23:46:52 +0200

    patched executor

--- executor/service.go
+++ executor/service.go
@@ -3,7 +3,9 @@ package executor
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"github.com/viant/datly/shared"
+	"strings"
 	"sync"
 )
 
@@ -21,8 +23,20 @@ func (e *Executor) Exec(ctx context.Context, session *Session) error {
 		return err
 	}
 
-	db, err := session.View.Db()
+	if err = e.exec(ctx, session, data); err != nil {
+		return err
+	}
 
+	printer.Flush()
+	return err
+}
+
+func (e *Executor) exec(ctx context.Context, session *Session, data []*SQLStatment) error {
+	if len(data) == 0 {
+		return nil
+	}
+
+	db, err := session.View.Db()
 	if err != nil {
 		return err
 	}
@@ -36,7 +50,7 @@ func (e *Executor) Exec(ctx context.Context, session *Session) error {
 	wg := &sync.WaitGroup{}
 	wg.Add(len(data))
 	for i := range data {
-		e.execData(ctx, wg, tx, data[i], errors)
+		e.execData(ctx, wg, tx, data[i], errors, session)
 	}
 
 	wg.Wait()
@@ -46,20 +60,27 @@ func (e *Executor) Exec(ctx context.Context, session *Session) error {
 		return err
 	}
 
-	printer.Flush()
-
 	return tx.Commit()
 }
 
-func (e *Executor) execData(ctx context.Context, wg *sync.WaitGroup, tx *sql.Tx, data *SQLStatment, errors *shared.Errors) {
+func (e *Executor) execData(ctx context.Context, wg *sync.WaitGroup, tx *sql.Tx, data *SQLStatment, errors *shared.Errors, session *Session) {
 	defer wg.Done()
-	err := e.executeStatement(ctx, tx, data)
+	if strings.TrimSpace(data.SQL) == "" {
+		return
+	}
+
+	err := e.executeStatement(ctx, tx, data, session)
 	if err != nil {
 		errors.Append(err)
 	}
 }
 
-func (e *Executor) executeStatement(ctx context.Context, tx *sql.Tx, stmt *SQLStatment) error {
+func (e *Executor) executeStatement(ctx context.Context, tx *sql.Tx, stmt *SQLStatment, session *Session) error {
 	_, err := tx.ExecContext(ctx, stmt.SQL, stmt.Args...)
+	if err != nil {
+		session.View.Logger.LogDatabaseErr(stmt.SQL, err)
+		err = fmt.Errorf("error occured while connecting to database")
+	}
+
 	return err
 }
