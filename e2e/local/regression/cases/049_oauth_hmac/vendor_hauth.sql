7c7ad5c6fc14cc5faaa2c6f47673787281a06b4e
awitas <awitas@vindicotech.com>
2023-03-24 06:10:05 -0700

    added hmac example

---
+++ e2e/local/regression/cases/049_oauth_hmac/vendor_hauth.sql
@@ -0,0 +1,14 @@
+/* {"URI":"hauth/vendors/{vendorID}"} */
+
+#set($_ = $Jwt<string>(Header/Authorization).WithCodec(JwtClaim).WithStatusCode(401))
+#set($_ = $Authorization  /*
+    {"Type": "Authorizer", "StatusCode": 403}
+    SELECT Authorized /* {"DataType":"bool"} */
+    FROM (SELECT IS_VENDOR_AUTHORIZED($Jwt.UserID, $vendorID) AS Authorized) t
+    WHERE Authorized
+*/)
+
+SELECT vendor.*,
+       products.* EXCEPT VENDOR_ID
+FROM (SELECT CAST($Jwt.FirstName AS CHAR) AS FIRST_NAME, t.* FROM VENDOR t WHERE t.ID = $vendorID ) vendor
+    JOIN (SELECT * FROM PRODUCT t) products ON products.VENDOR_ID = vendor.ID
