70d66011e44e2482808882854dbb98fbd6fb14c4
awitas <awitas@vindicotech.com>
2023-04-11 10:29:57 -0700

    updated test case

--- e2e/local/regression/cases/048_json_persist_with_type/custom_unmarshall_velty.sql
+++ e2e/local/regression/cases/048_json_persist_with_type/custom_unmarshall_velty.sql
@@ -1,8 +1,8 @@
 /* {"URI":"custom-unmarshall-velty","Method":"PUT","ResponseBody":{"From":"Preference"}} */
 
 import (
-	"regression/cases/048_json_persist.Preference"
-	"regression/cases/048_json_persist.Foo" as "com.class.abc.Foo"
+	"regression/cases/048_json_persist_with_type.Preference"
+	"regression/cases/048_json_persist_with_type.Foo" as "com.class.abc.Foo"
 )
 
 #set($_ = $Preference<*Preference>(body/))
