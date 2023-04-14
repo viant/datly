a05b76f7375ce5f7298ad5ea9fa8f432d251bca5
awitas <adrianwit@gmail.com>
2023-04-14 12:57:48 -0700

    added variables case

--- e2e/local/regression/cases/055_variables/vars.sql
+++ e2e/local/regression/cases/055_variables/vars.sql
@@ -1,5 +1,15 @@
-/* {"URI":"vars/",   "Const": { "Var1": "setting1", "Var2": "setting2" }} */
+/*
+ {
+  "URI": "vars/",
+  "Const": {
+    "Vendor": "VENDOR",
+    "Product": "PRODUCT",
+    "Var1": "setting1",
+    "Var2": "setting2"
+  }
+}
+ */
 SELECT main.*
 FROM (
-    SELECT Key1, Key2 FROM (SELECT '$Var1' AS Key1, '$Var2' AS Key2) t
+    SELECT Key1, Key2 FROM (SELECT '$Var1 - $Vendor' AS Key1, '$Var2 - $Product' AS Key2) t
 ) main
