7af9f7a4c93ed85f5060e1f5abb4ac4fcb80b83e
awitas <adrianwit@gmail.com>
2023-05-09 11:14:07 -0700

    structqlv2 added structql params

---
+++ poc/post.sql
@@ -0,0 +1,38 @@
+/* {"Method":"PATCH","ResponseBody":{"From":"Campaign"}} */
+
+import (
+	"/Users/awitas/go/src/github.com/viant/datly/poc/xxx/.Campaign"
+	"/Users/awitas/go/src/github.com/viant/datly/poc/xxx/.Entity"
+)
+
+
+#set($_ = $Campaign<*Campaign>(body/Entity))
+    
+#set($_ = $prevCampaign<[]*Campaign> /*
+  SELECT * FROM CI_CAMPAIGN  WHERE  ID = $Campaign.Id
+*/)
+  
+#set($_ = $advertiserIds /* 
+  SELECT ARRAY_AGG(Id) AS Values FROM `/Advertiser/`
+*/)   
+
+#set($_ = $prevAdvertiser /* 
+  SELECT * FROM CI_ADVERTISER
+  WHERE  ID IN ( $advertiserIds.Values )
+*/)
+    
+    
+$sequencer.Allocate("CI_CAMPAIGN", $Campaign, "Id")
+$sequencer.Allocate("CI_ADVERTISER", $Campaign, "Advertiser/Id")
+
+    
+#set($campaignById = $prevCampaign.IndexBy("Id"))
+#set($advertiserById = $prevAdvertiser.IndexBy("Id"))
+
+#if($Unsafe.Campaign)
+  #if(($campaignById.HasKey($Unsafe.Campaign.Id) == true))
+    $sql.Update($Campaign, "CI_CAMPAIGN");
+  #else
+    $sql.Insert($Campaign, "CI_CAMPAIGN");
+  #end
+#end
