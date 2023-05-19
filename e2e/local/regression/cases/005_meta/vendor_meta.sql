8f8bc1c74d559d315b91c88a2aaa22f5b0220e3c
awitas <adrianwit@gmail.com>
2023-05-19 12:09:37 -0700

    patched multi exclusion

--- e2e/local/regression/cases/005_meta/vendor_meta.sql
+++ e2e/local/regression/cases/005_meta/vendor_meta.sql
@@ -1,8 +1,8 @@
-/* {"URI":"meta/vendors/", "CaseFormat":"lu"} */
+/* {"URI":"meta/vendors/", "CaseFormat":"lc"} */
 SELECT vendor.* /* {"Style":"Comprehensive", "Field":"Data"}  */,
        products.* EXCEPT VENDOR_ID,
        Meta.* /* {"Kind": "record"} */
 FROM (SELECT t.* FROM VENDOR t WHERE 1=1  ) vendor
-    JOIN (SELECT * FROM PRODUCT t) products  ON products.VENDOR_ID = vendor.ID
+    JOIN (SELECT ID, NAME AS my_name /* {"IgnoreCaseFormatter":true} */, VENDOR_ID   FROM PRODUCT t) products  ON products.VENDOR_ID = vendor.ID
     JOIN (SELECT   CAST(1 + (COUNT(1) / $View.Limit) AS SIGNED)  AS PAGE_CNT, COUNT(1) AS CNT
     FROM ($View.vendor.SQL)  t ) AS Meta ON 1=1
