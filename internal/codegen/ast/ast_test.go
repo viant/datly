b973205d293a7ad9b73c4bcec98cd789f63e858b
awitas <adrianwit@gmail.com>
2023-06-21 11:41:13 -0700

    work in progress - abstracted out plugin info/artifact

--- internal/codegen/ast/ast_test.go
+++ internal/codegen/ast/ast_test.go
@@ -121,48 +121,4 @@ func TestBlock_Stringify(t *testing.T) {
 		actual := builder.String()
 		assert.EqualValues(t, testCase.expect, strings.TrimSpace(actual))
 	}
-
-	/*
-		#set($inited = $Campaign.Init($CurCampaign, $Acl, $Features))
-
-		#set($info = $Campaign.Validate($CurCampaign, $CurAdvertiser, $NewCreatives, $Acl, $Features, $session))
-		#if($info.HasError ==  true)
-		$response.StatusCode(401)
-		$response.Failf("%v",$info.Error)
-		#end
-
-
-
-
-		$sequencer.Allocate("CI_CAMPAIGN", $Campaign, "Id")
-		$sequencer.Allocate("CI_CAMPAIGN_FLIGHT", $Campaign, "Flights/Id")
-		$sequencer.Allocate("CI_CAMPAIGN_CREATIVE", $Campaign, "CampaignCreative/Id")
-		$sequencer.Allocate("CI_CAMPAIGN_BID_MULTIPLIER", $Campaign, "BidOverride/Id")
-
-
-
-		#set($capModified = $Campaign.HasCapChange($CurCampaign, $Features))
-		#if($capModified == true)
-		#set($msg = $messageBus.Message("aws/topic/us-west-1/datly-e2e-campaign", $Campaign.Id))
-		#set($confirmation = $messageBus.Push($msg))
-		$logger.Printf("confirmation:%v", $confirmation.MessageID)
-		#end
-
-		#set($FlightsById = $CurFlights.IndexBy("Id"))
-		#set($CampaignCreativeById = $CurCampaignCreative.IndexBy("Id"))
-		#set($BidOverrideById = $CurBidOverride.IndexBy("Id"))
-
-
-		#if($Campaign.Has.Id == true)
-		$sql.Update($Campaign, "CI_CAMPAIGN");
-		#else
-		$sql.Insert($Campaign, "CI_CAMPAIGN");
-		#end
-
-		#foreach($recFlight in $Unsafe.Campaign.Flights)
-		#if(($FlightsById.HasKey($recFlight.Id) == true))
-		$sql.Update($recFlight, "CI_CAMPAIGN_FLIGHT");
-		#else
-	*/
-
 }
