97ecbdac93b39ec3d9aa65e527762570f1a21da5
awitas <adrianwit@gmail.com>
2023-07-23 09:17:15 -0700

    work in progress

---
+++ cmd/datly/datly.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"github.com/google/gops/agent"
+	"github.com/viant/datly/cmd"
+	"github.com/viant/datly/cmd/env"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+var (
+	Version      = "development"
+	BuildTimeInS string
+)
+
+func init() {
+	if BuildTimeInS != "" {
+		seconds, err := strconv.Atoi(BuildTimeInS)
+		if err != nil {
+			panic(err)
+		}
+
+		env.BuildTime = time.Unix(int64(seconds), 0)
+	}
+}
+
+type ConsoleWriter struct{}
+
+func (c *ConsoleWriter) Write(data []byte) (n int, err error) {
+	fmt.Println(string(data))
+	return len(data), nil
+}
+
+func main() {
+	fmt.Printf("[INFO] Build time: %v\n", env.BuildTime.String())
+
+	go func() {
+		if err := agent.Listen(agent.Options{}); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	server, err := cmd.New(Version, os.Args[1:], &ConsoleWriter{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if server != nil {
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal(err.Error())
+		}
+	}
+}
