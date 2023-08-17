f46864d0a31550475b3cbe184f4c3ff6a65c65d2
awitas <adrianwit@gmail.com>
2023-08-17 09:10:11 -0700

    inline binding parameter for error logging

--- logger/adapter.go
+++ logger/adapter.go
@@ -3,7 +3,10 @@ package logger
 import (
 	"fmt"
 	"github.com/viant/datly/shared"
+	"github.com/viant/toolbox"
 	"os"
+	"reflect"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -87,6 +90,32 @@ func (l *Adapter) Inherit(adapter *Adapter) {
 }
 
 func (l *Adapter) LogDatabaseErr(SQL string, err error, args ...interface{}) {
+	for _, arg := range args {
+		if reflect.TypeOf(arg).Kind() == reflect.Ptr {
+			if arg != nil {
+				arg = reflect.ValueOf(arg).Elem().Interface()
+			}
+		}
+
+		if arg == nil {
+			SQL = strings.Replace(SQL, "?", " NULL ", 1)
+			continue
+		}
+		switch actual := arg.(type) {
+		case int:
+			SQL = strings.Replace(SQL, "?", strconv.Itoa(actual), 1)
+		case int64:
+			SQL = strings.Replace(SQL, "?", strconv.Itoa(int(actual)), 1)
+		case float64:
+			SQL = strings.Replace(SQL, "?", strconv.FormatFloat(actual, 'f', 5, 32), 1)
+		case bool:
+			SQL = strings.Replace(SQL, "?", strconv.FormatBool(actual), 1)
+
+		default:
+			val := toolbox.AsString(arg)
+			SQL = strings.Replace(SQL, "?", `'`+val+`'`, 1)
+		}
+	}
 	fmt.Printf(fmt.Sprintf("error occured while executing SQL: %v, SQL: %v, params: %v\n", err, strings.ReplaceAll(SQL, "\n", "\\n"), args))
 }
 
