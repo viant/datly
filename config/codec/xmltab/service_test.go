8aeed3d231227af74c4f2780d475693996c2fff0
awitas <adrianwit@gmail.com>
2023-09-05 08:45:11 -0700

    updated test cases

--- config/codec/xmltab/service_test.go
+++ config/codec/xmltab/service_test.go
@@ -40,6 +40,47 @@ func TestService_Transfer(t *testing.T) {
 
 			},
 		},
+		{
+			description: "slice with ptr struct",
+			getData: func() interface{} {
+				type Foo struct {
+					Id   int
+					Name string
+					F    float64
+					II   int
+				}
+
+				return []*Foo{
+					{
+						Id:   1,
+						II:   12,
+						F:    12312.3,
+						Name: "abc",
+					},
+					{
+						Id:   2,
+						II:   4444,
+						F:    2.3,
+						Name: "xyz",
+					},
+				}
+
+			},
+		},
+
+		{
+			description: "basic slice",
+			getData: func() interface{} {
+				type Foo struct {
+					Id   int
+					Name string
+					F    float64
+					II   int
+				}
+
+				return []Foo{}
+			},
+		},
 	}
 
 	srv := New()
@@ -47,6 +88,7 @@ func TestService_Transfer(t *testing.T) {
 		transfer, err := srv.Transfer(testCase.getData())
 		assert.Nil(t, err, testCase.description)
 		data, _ := json.Marshal(transfer)
+		fmt.Printf("%s --- \n", testCase.description)
 		fmt.Printf("%s\n", data)
 	}
 
