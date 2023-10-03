18c7baf127b8dc3bd658ba5879a3943867010e51
adranwit <adrianwit@gmail.com>
2023-10-03 07:43:11 -0700

    refactoring gateway

--- view/definition.go
+++ view/definition.go
@@ -79,7 +79,6 @@ func (d *TypeDefinition) Init(ctx context.Context, lookupType xreflect.LookupTyp
 		if d.Schema.DataType != d.Name && d.Name != "" {
 			d.Schema.Name = d.Name
 		}
-		fmt.Printf("INIT: %s %s\n", d.Schema.TypeName())
 		if err := d.Schema.InitType(lookupType, d.Ptr); err != nil {
 			return fmt.Errorf("invalid type def: %s (%s), %w", d.Name, d.DataType, err)
 		}
