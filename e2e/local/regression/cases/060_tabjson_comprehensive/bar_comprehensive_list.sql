d910f8ddd004298452b9bf7200daf8464f82d1d4
adranwit <adrianwit@gmail.com>
2024-11-11 12:33:23 -0800

    - patched e2e test cases - removing Style logic

--- e2e/local/regression/cases/060_tabjson_comprehensive/bar_comprehensive_list.sql
+++ e2e/local/regression/cases/060_tabjson_comprehensive/bar_comprehensive_list.sql
@@ -1,6 +1,8 @@
 /* {"URI":"comprehensive/bars/", "DataFormat":"tabular", "TabularJSON":{"FloatPrecision":"-1"}} */
 
-#set( $_ = $Data<?>(output/view).WithTag('anonymous:"true"'))
+
+#set( $_ = $Data<?>(output/view).Cardinality('One').WithTag('anonymous:"true"'))
+
 
 SELECT bar.*
 FROM (SELECT * FROM BAR t ) bar
