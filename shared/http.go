5811a0a6ae4644cd79695a850411e166c73583c9
adranwit <adrianwit@gmail.com>
2025-11-05 05:10:41 -0800

    added tx option and added support for multi part

--- shared/http.go
+++ shared/http.go
@@ -3,22 +3,35 @@ package shared
 import (
 	"bytes"
 	"io"
+	"mime"
 	"net/http"
+	"strings"
 )
 
 // CloneHTTPRequest clones http request
 func CloneHTTPRequest(request *http.Request) (*http.Request, error) {
-	var data []byte
-	var err error
+	// Shallow clone; special-case multipart to avoid buffering entire body
 	ret := *request
 	ret.URL = request.URL
-	if request.Body != nil {
-		if data, err = readRequestBody(request); err != nil {
-			return nil, err
-		}
-		ret.Body = io.NopCloser(bytes.NewReader(data))
+
+	if request.Body == nil {
+		return &ret, nil
 	}
-	return &ret, err
+
+	// Detect multipart/form-data; avoid reading/consuming body
+	if isMultipartRequest(request) {
+		// share the same Body; caller must ensure only one reader consumes it
+		ret.Body = request.Body
+		return &ret, nil
+	}
+
+	// Non-multipart: safe full read, reset both original and clone bodies
+	data, err := readRequestBody(request)
+	if err != nil {
+		return nil, err
+	}
+	ret.Body = io.NopCloser(bytes.NewReader(data))
+	return &ret, nil
 }
 
 func readRequestBody(request *http.Request) ([]byte, error) {
@@ -29,4 +42,19 @@ func readRequestBody(request *http.Request) ([]byte, error) {
 	_ = request.Body.Close()
 	request.Body = io.NopCloser(bytes.NewReader(data))
 	return data, err
+}
+
+func isMultipartRequest(r *http.Request) bool {
+	if r == nil || r.Header == nil {
+		return false
+	}
+	ct := r.Header.Get("Content-Type")
+	if ct == "" {
+		return false
+	}
+	mediaType, _, err := mime.ParseMediaType(ct)
+	if err != nil {
+		return strings.Contains(strings.ToLower(ct), "multipart/form-data")
+	}
+	return strings.EqualFold(mediaType, "multipart/form-data")
 }
