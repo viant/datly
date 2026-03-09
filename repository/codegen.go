2e260feadf18af513fb1bb0ff7b0813653e05094
adranwit <adrianwit@gmail.com>
2026-03-09 14:45:22 -0700

    added dynamic grouping

--- repository/codegen.go
+++ repository/codegen.go
@@ -13,7 +13,6 @@ import (
 	"github.com/viant/tagly/format/text"
 	"github.com/viant/toolbox/data"
 	"github.com/viant/xreflect"
-	"os"
 	"path"
 	"reflect"
 	"strconv"
@@ -30,27 +29,6 @@ func (c *Component) GenerateOutputCode(ctx context.Context, withDefineComponent,
 	builder := strings.Builder{}
 	input := c.Input.Type.Type()
 	registry := c.TypeRegistry()
-	if os.Getenv("DATLY_DEBUG_CODEGEN") != "" {
-		outputViewSchema := "<nil>"
-		outputViewType := "<nil>"
-		if viewParameter := c.Output.Type.Parameters.LookupByLocation(state.KindOutput, "view"); viewParameter != nil && viewParameter.Schema != nil {
-			outputViewSchema = viewParameter.Schema.Name
-			if viewParameter.Schema.Type() != nil {
-				outputViewType = viewParameter.Schema.Type().String()
-			}
-		}
-		defs := 0
-		if c.View != nil {
-			defs = len(c.View.TypeDefinitions())
-		}
-		fmt.Printf("[DATLY_CODEGEN] uri=%s method=%s view=%s defs=%d outputViewSchema=%s outputViewType=%s package=%s\n",
-			c.URI, c.Method, func() string {
-				if c.View == nil {
-					return "<nil>"
-				}
-				return c.View.Name
-			}(), defs, outputViewSchema, outputViewType, c.Output.Type.Package)
-	}
 
 	if viewParameter := c.Output.Type.Parameters.LookupByLocation(state.KindOutput, "view"); viewParameter != nil {
 		aTag := &tags.Tag{}
@@ -77,9 +55,6 @@ func (c *Component) GenerateOutputCode(ctx context.Context, withDefineComponent,
 			aTag.SummarySQL = tags.ViewSQLSummary(tags.NewViewSQL(tmpl.Summary.Source, ""))
 		}
 		viewParameter.Tag = string(aTag.UpdateTag(reflect.StructTag(viewParameter.Tag)))
-		if c.View != nil && c.View.Schema != nil && c.View.Schema.Name != "" && !strings.Contains(viewParameter.Tag, `typeName:"`) {
-			viewParameter.Tag = strings.TrimSpace(viewParameter.Tag + ` typeName:"` + c.View.Schema.Name + `"`)
-		}
 	}
 
 	output, _ := c.Output.Type.Parameters.ReflectType("", registry.Lookup, state.WithRelation(), state.WithSQL(), state.WithVelty(false))
